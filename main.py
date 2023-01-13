f1daf000f416fb1445884c3c4f051b56c872dc0a
nateraw <nxr9266@g.rit.edu>
2023-01-13 22:33:08 +0100

    :memo: update completion settings

--- main.py
+++ main.py
@@ -51,11 +51,11 @@ def response(text):
     response = openai.Completion.create(
         engine="text-davinci-002",
         prompt=text,
-        temperature=0.7,
+        temperature=0.9,
         max_tokens=60,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0,
+        presence_penalty=1.0,
     )
 
     return response.choices[0].text.strip()
